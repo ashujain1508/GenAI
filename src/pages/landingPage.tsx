3e3a4d2f321d97aec9ac5f04fe3a4b52631332ec
Aarushi Jain <jaarushi24@gmail.com>
2024-11-10 21:23:13 +0530

    cover photo

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,27 +1,20 @@
 import { Grid } from '@mui/material';
 import React from 'react';
-import Navigation from '../components/Navigation/Navigation';
 import Information from '../components/Information/Information';
 import AccountDetails from '../components/AccountDetails';
 import MultiProductCard from '../components/Product/MultiProductCard';
+
 const LandingPage: React.FC = () => {
     return(
         <Grid container>
-            <Grid item xs={12} md={12} >
-
-                <Navigation/>   
-              
-                
+            <Grid item xs={12} md={12}>
+                <Information/>
             </Grid>
             <Grid item xs={12} md={12}>
-            <Information/>
-          
+                <AccountDetails/>
             </Grid>
             <Grid item xs={12} md={12}>
-            <AccountDetails/>
-            </Grid>
-            <Grid item xs={12} md={12}>
-            <MultiProductCard/>
+                <MultiProductCard/>
             </Grid>
         </Grid>
     )
